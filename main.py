33f5066fe79004efaa147f712af73c159611c4cd
phamducthien4325 <23020160@vnu.edu.>
2025-05-17 16:23:30 +0700

    Add nearest and bilinear interpolation functions with corresponding image outputs

--- main.py
+++ main.py
@@ -34,6 +34,22 @@ def nearest_interpolation(image, dimension):
 
     return new_image
 
+def nearest_interpolation_vectorized(image, dimension):
+    h_out, w_out = dimension
+    h_in, w_in = image.shape[:2]
+
+    scale_y = h_in / h_out
+    scale_x = w_in / w_out
+
+    i = np.arange(h_out)
+    j = np.arange(w_out)
+    jj, ii = np.meshgrid(j, i) 
+
+    row = (ii * scale_y).astype(int)
+    col = (jj * scale_x).astype(int)
+
+    return image[row, col]
+
 def bilinear_interpolation(image, dimension):
     height = image.shape[0]
     width = image.shape[1]
@@ -74,7 +90,7 @@ def main():
     images_list = {}
 
     # Read Image
-    img, size, dimension = read_image("./images/butterfly.png")
+    img, size, dimension = read_image("./images/planet.jpg")
     print(f"Image size is: {size}")
     images_list['Original Image'] = img
 
@@ -84,6 +100,7 @@ def main():
     print(f"Smalled Image size is: {resized_img.shape}")
     images_list['Smalled Image'] = resized_img
 
+    # Nearest Interpolation
     nn_img_algo = nearest_interpolation(resized_img, dimension)
     nn_img_algo = cv2.cvtColor(np.array(nn_img_algo), cv2.COLOR_BGR2RGB)
     nn_img_algo = Image.fromarray(nn_img_algo.astype('uint8'))
@@ -94,16 +111,38 @@ def main():
     nn_img = Image.fromarray(nn_img.astype('uint8'))
     nn_img.save("./images/opencv_nearest.png")
 
-    fig, axs = plt.subplots(1, 4)
+    fig1, axs1 = plt.subplots(1, 4)
     # fig.suptitle('25 Percent of the original size', fontsize=16)
-    axs[0].set_title('Original 256x256 Image')
-    axs[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    axs[1].set_title('64x64 Image')
-    axs[1].imshow(cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB))
-    axs[2].set_title('256x256 Image with Nearest')
-    axs[2].imshow(nn_img_algo)
-    axs[3].set_title('256x256 Image with OpenCV\'s Nearest')
-    axs[3].imshow(nn_img)
+    axs1[0].set_title('Original 256x256 Image')
+    axs1[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    axs1[1].set_title('64x64 Image')
+    axs1[1].imshow(cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB))
+    axs1[2].set_title('256x256 Image with Nearest')
+    axs1[2].imshow(nn_img_algo)
+    axs1[3].set_title('256x256 Image with OpenCV\'s Nearest')
+    axs1[3].imshow(nn_img)
+
+    # Bilinear Interpolation
+    bl_img_algo = bilinear_interpolation(resized_img, dimension)
+    bl_img_algo = cv2.cvtColor(np.array(bl_img_algo, dtype=np.uint8), cv2.COLOR_BGR2RGB)
+    bl_img_algo = Image.fromarray(bl_img_algo.astype('uint8'))
+    bl_img_algo.save("./images/my_bilinear.png")
+
+    bl_img = image_change_scale(resized_img, dimension, interpolation=cv2.INTER_LINEAR)
+    bl_img = cv2.cvtColor(np.array(bl_img), cv2.COLOR_BGR2RGB)
+    bl_img = Image.fromarray(bl_img.astype('uint8'))
+    bl_img.save("./images/opencv_bilinear.png")
+
+    fig2, axs2 = plt.subplots(1, 4)
+    # fig.suptitle('25 Percent of the original size', fontsize=16)
+    axs2[0].set_title('Original 256x256 Image')
+    axs2[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    axs2[1].set_title('64x64 Image')
+    axs2[1].imshow(cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB))
+    axs2[2].set_title('256x256 Image with Bilinear')
+    axs2[2].imshow(bl_img_algo)
+    axs2[3].set_title('256x256 Image with OpenCV\'s Bilinear')
+    axs2[3].imshow(bl_img)
     plt.show()
 
 if __name__ == "__main__":
